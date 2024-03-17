d2edccb3a2f62e45017f115e6198804f6822a4b8
Stefan <stefan.prechtl@everii.io>
2024-03-17 15:43:33 +0100

    feat: getting and incrementing a user works

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 type UserPacks struct {
 	id        string
 	Username  string
-	PackCount int
+	PackCount uint
 }
 
 func main() {
@@ -34,9 +34,14 @@ func main() {
 	defer db.Close()
 
 	r := gin.Default()
-	r.GET("/:username", func(ctx *gin.Context) {
+	r.GET("/add/:username", func(ctx *gin.Context) {
 		increase(ctx, db)
 	})
+
+	r.GET("/:username", func(ctx *gin.Context) {
+		get(ctx, db)
+	})
+
 	r.Run()
 }
 
@@ -55,21 +60,82 @@ func loadDatabaseUrl() string {
 	return connectionString
 }
 
+func get(c *gin.Context, db *sql.DB) {
+	username := c.Param("username")
+	user, err := getUserPackByUsername(username, db)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			createdUser, creationError := createUser(username, db)
+
+			if creationError != nil {
+				log.Fatalf("Error creating user: %v", err)
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server error while creating new user"})
+				return
+			}
+			log.Printf("Created new user: %s", username)
+			user = createdUser
+		} else {
+			log.Fatalf("Error scanning row: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server error"})
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, user)
+}
+
 func increase(c *gin.Context, db *sql.DB) {
 	username := c.Param("username")
+	user, err := getUserPackByUsername(username, db)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			log.Fatalf("Error increasing row: %v", err)
+			c.JSON(http.StatusNotFound, gin.H{"error": "User not found try creating it first by getting it's score before increasing."})
+		}
+
+		log.Fatalf("Error scanning row: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server error"})
+		return
+	}
+
+	user, err = updateScore(username, user.PackCount+1, db)
+	if err != nil {
+		log.Fatalf("Error creating user: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server error"})
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
+}
+
+func createUser(username string, db *sql.DB) (UserPacks, error) {
+	_, err := db.Exec("INSERT INTO user_packs (username, pack_count) VALUES ($1, $2)", username, 0)
+	if err != nil {
+		return UserPacks{}, err
+	}
+
+	return getUserPackByUsername(username, db)
+}
+
+func updateScore(username string, newCount uint, db *sql.DB) (UserPacks, error) {
+	_, err := db.Exec("UPDATE user_packs SET pack_count=$1 WHERE username=$2", newCount, username)
+
+	if err != nil {
+		return UserPacks{}, err
+	}
+	return getUserPackByUsername(username, db)
+}
+
+func getUserPackByUsername(username string, db *sql.DB) (UserPacks, error) {
 	row := db.QueryRow("SELECT * FROM user_packs WHERE username=$1", username)
 
 	var res UserPacks
 	err := row.Scan(&res.id, &res.Username, &res.PackCount)
+
 	if err != nil {
-		if err == sql.ErrNoRows {
-			log.Printf("No rows were returned for username: %s", username)
-			c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
-			return
-		}
-		log.Fatalf("Error scanning row: %v", err)
+		return UserPacks{}, err
 	}
 
-	log.Println(res)
-	c.JSON(http.StatusOK, res)
+	return res, nil
 }
